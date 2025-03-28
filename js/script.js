56b6a766e4614f961060ad69a8f7f09336e8cd60
Elisabeth Kaseda <72435921+elisabethkaseda@users.noreply.github.com>
2025-03-28 10:49:05 -0500

    fix scrolling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const container = document.querySelector('.weather');
 
-// This APi call is based on the way we learned it in Front-End Development class; apparently this is a spaghetti code way of doing it and it should be done using async/await try/catch and returning data (this current version returns undefined), but it's good enough for now. Will fix later when converting to a React site, after I've had a few weeks to study JavaScript and React more.
+// This API call is mostly based on the way we learned it in Front-End Development class; may change to async/await later so it actually returns
 function getCurrentWeather() {
     fetch('https://api.open-meteo.com/v1/forecast?latitude=35.6895&longitude=139.6917&current=temperature_2m,is_day,weather_code&timezone=auto')
     .then(
@@ -10,7 +10,7 @@ function getCurrentWeather() {
     )
     .then(
         response => {
-            console.log(response);
+            // console.log(response);
             displayWeather(response);
         }
     )
@@ -24,10 +24,16 @@ function getCurrentWeather() {
 }
 
 function displayWeather(dataObject) {
+    let weatherCode = dataObject.current.weather_code;
+    console.log(weatherCode);
+    let weatherIcon;
+
+
+
     container.innerHTML = `<span>${dataObject.current.weather_code}/${dataObject.current.temperature_2m}&deg;C</span>`
 }
 
-const setTheme = theme => document.documentElement.className = theme;
-setTheme('light');
+// const setTheme = theme => document.documentElement.className = theme;
+// setTheme('light');
 
 getCurrentWeather();
